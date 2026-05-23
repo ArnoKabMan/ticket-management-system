e460a5afc34b0eb0b5208ba58f19585a8fea1a6d
arnokabman <manzi.arnaud@gmail.com>
2026-05-23 15:09:10 +0200

    first

--- backend/incident-service/src/main/java/org/arnoproject/ticketmanagementsystem/TicketManagementSystemApplication.java
+++ backend/incident-service/src/main/java/org/arnoproject/ticketmanagementsystem/TicketManagementSystemApplication.java
@@ -2,12 +2,17 @@ package org.arnoproject.ticketmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TicketManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketManagementSystemApplication.class, args);
+    }
+    @GetMapping("/health")
+    public String health() {
+        return "Incident Service is running";
     }
 
 }
